15b163b30e337e0b82aa1a586bbe3d8a421f3080
Javier Perez <javier21perez98@gmail.com>
2021-05-12 16:59:32 -0700

    created file to hold table

---
+++ src/components/Table.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+// import API from "../utils/API";
+
+class Table extends Component {
+	render() {
+		return (
+			<>
+				<input
+					className="form-control form-control-lg mx-auto my-5 col-md-4"
+					type="text"
+					placeholder="Search User"
+				/>
+				<div className="table-responsive">
+					<table className="table">
+						<thead className="thead-light text-center">
+							<tr>
+								<th scope="col">Image</th>
+								<th scope="col">Name</th>
+								<th scope="col">Phone</th>
+								<th scope="col">Email</th>
+								<th scope="col">DOB: M/DD/YY</th>
+							</tr>
+						</thead>
+						<tbody className="text-center">
+							<tr>
+								<th>Pic</th>
+								<td>Mark</td>
+								<td>(364)645-3928</td>
+								<td>email@gmail.com</td>
+								<td>4/12/89</td>
+							</tr>
+							<tr>
+								<th>Pic</th>
+								<td>Thornton</td>
+								<td>(364)645-3928</td>
+								<td>email@gmail.com</td>
+								<td>2/6/98</td>
+							</tr>
+							<tr>
+								<th>Pic</th>
+								<td>the Bird</td>
+								<td>(364)645-3928</td>
+								<td>email@gmail.com</td>
+								<td>8/20/92</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</>
+		);
+	}
+}
+
+export default Table;
