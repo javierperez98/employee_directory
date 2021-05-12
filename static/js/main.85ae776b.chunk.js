73ab43ae7203eec29fd2a1edd10fa4e9c1d618fb
Javier Perez <javier21perez98@gmail.com>
2021-05-12 14:44:40 -0700

    Updates

---
+++ static/js/main.85ae776b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemployee_directory=this.webpackJsonpemployee_directory||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var c=n(1),o=n.n(c),r=n(3),i=n.n(r),s=n(0);var u=function(){return Object(s.jsx)("h1",{children:"Hello"})},d=function(e){e&&e instanceof Function&&n.e(3).then(n.bind(null,10)).then((function(t){var n=t.getCLS,c=t.getFID,o=t.getFCP,r=t.getLCP,i=t.getTTFB;n(e),c(e),o(e),r(e),i(e)}))};i.a.render(Object(s.jsx)(o.a.StrictMode,{children:Object(s.jsx)(u,{})}),document.getElementById("root")),d()}},[[9,1,2]]]);
+//# sourceMappingURL=main.85ae776b.chunk.js.map
