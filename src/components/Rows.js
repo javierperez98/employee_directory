8e82cb07112c35778e2e964d2f78909dc6b3fcfb
Javier Perez <javier21perez98@gmail.com>
2021-05-13 17:37:06 -0700

    created map function to create rows using the api info

---
+++ src/components/Rows.js
@@ -0,0 +1,28 @@
+import React from "react";
+import API from "../utils/Api";
+
+API.search().then((res) => {
+	return res.data.results;
+});
+
+function Rows(props) {
+	return (
+		<tbody className="text-center">
+			{props.arr.map((info) => (
+				<tr>
+					<td>
+						<img src={info.picture.thumbnail} alt={info.name.first}></img>
+					</td>
+					<td>
+						{info.name.first} {info.name.last}
+					</td>
+					<td>{info.phone}</td>
+					<td>{info.email}</td>
+					<td>{info.dob.date}</td>
+				</tr>
+			))}
+		</tbody>
+	);
+}
+
+export default Rows;
