5815076e07a24e6c52c0b7c2683f5b7f4102588d
Javier Perez <javier21perez98@gmail.com>
2021-05-14 00:31:24 -0700

    created function to search for a user by name

--- src/App.js
+++ src/App.js
@@ -4,25 +4,53 @@ import API from "./utils/Api";
 
 function App() {
 	const [List, setList] = useState([]);
+	const [Original, setOriginal] = useState([]);
 	const [Find, setFind] = useState("");
 
 	useEffect(() => {
 		API.search().then((res) => {
 			setList(res.data.results);
+			setOriginal(res.data.results);
 		});
 	}, []);
 
-	const handleFind = (event) => setFind(event.target.value);
-	console.log(Find);
+	const handleFind = (event) => {
+		setFind(event.target.value);
+		search();
+	};
+
+	const search = () => {
+		const result = List.filter((user) =>
+			(user.name.first + " " + user.name.last)
+				.toLowerCase()
+				.includes(Find.toLowerCase())
+		);
+		if (result.length > 0) {
+			setList(result);
+		} else {
+			setList(Original);
+		}
+	};
+
+	const reset = () => {
+		setList(Original);
+		setFind("");
+	};
+
 	return (
 		<>
-			<input
-				value={Find}
-				onChange={handleFind}
-				className="form-control form-control-lg mx-auto my-5 col-md-4"
-				type="text"
-				placeholder="Search User"
-			/>
+			<div className="d-flex flex-row">
+				<input
+					value={Find}
+					onChange={handleFind}
+					className="form-control form-control-lg mx-auto my-5"
+					type="text"
+					placeholder="Search User"
+				/>
+				<button type="button" onClick={reset} className="btn btn-secondary my-5">
+					Reset
+				</button>
+			</div>
 			<div className="table-responsive">
 				<table className="table">
 					<thead className="thead-light text-center">
