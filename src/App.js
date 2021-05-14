55375e896d67d9603c762f60a65378910ee855c6
Javier Perez <javier21perez98@gmail.com>
2021-05-13 17:36:17 -0700

    created hooks to display componets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from "react";
-import Table from "./components/Table";
+import React, { useState, useEffect } from "react";
+import Rows from "./components/Rows";
+import API from "./utils/Api";
 
 function App() {
-	return <Table />;
+	const [List, setList] = useState([]);
+	useEffect(() => {
+		API.search().then((res) => {
+			setList(res.data.results);
+		});
+	}, []);
+
+	return (
+		<>
+			<input
+				className="form-control form-control-lg mx-auto my-5 col-md-4"
+				type="text"
+				placeholder="Search User"
+			/>
+			<div className="table-responsive">
+				<table className="table">
+					<thead className="thead-light text-center">
+						<tr>
+							<th scope="col">Image</th>
+							<th scope="col">Name</th>
+							<th scope="col">Phone</th>
+							<th scope="col">Email</th>
+							<th scope="col">DOB: M/DD/YY</th>
+						</tr>
+					</thead>
+					<Rows arr={List} />
+				</table>
+			</div>
+		</>
+	);
 }
 
 export default App;
