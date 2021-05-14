baaaca768adb5d5cef3835c1e4ec91318fdc66db
Javier Perez <javier21perez98@gmail.com>
2021-05-13 19:50:28 -0700

    created function to handle search input text

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import API from "./utils/Api";
 
 function App() {
 	const [List, setList] = useState([]);
+	const [Find, setFind] = useState("");
+
 	useEffect(() => {
 		API.search().then((res) => {
 			setList(res.data.results);
 		});
 	}, []);
 
+	const handleFind = (event) => setFind(event.target.value);
+	console.log(Find);
 	return (
 		<>
 			<input
+				value={Find}
+				onChange={handleFind}
 				className="form-control form-control-lg mx-auto my-5 col-md-4"
 				type="text"
 				placeholder="Search User"
