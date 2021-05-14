a4e6a4101ecfdb3007e14905f30a5568506f9ef3
Javier Perez <javier21perez98@gmail.com>
2021-05-14 10:07:43 -0700

    added file to holder header jsx

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+	return (
+		<header className="bg-dark p-5 border-bottom border-primary">
+			<h1 className="text-light">Employee Directory</h1>
+		</header>
+	);
+}
+
+export default Header;
