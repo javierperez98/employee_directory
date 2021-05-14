ff1beb9258562a55db4fb07b4e1d299e86ad184c
Javier Perez <javier21perez98@gmail.com>
2021-05-13 18:33:20 -0700

    created a formate date function

---
+++ src/utils/formatDate.js
@@ -0,0 +1,12 @@
+function formatDate(date) {
+	var d = new Date(date),
+		month = "" + (d.getMonth() + 1),
+		day = "" + d.getDate(),
+		year = d.getFullYear().toString().substr(-2);
+
+	if (month.length < 2) month = "0" + month;
+	if (day.length < 2) day = "0" + day;
+
+	return [month, day, year].join("/");
+}
+export default formatDate;
