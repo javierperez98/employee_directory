10dd664cf6a7f1352719531c47eb4def0c3fe566
Javier Perez <javier21perez98@gmail.com>
2021-05-14 10:09:43 -0700

    updated to import header, footer and main

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
 	<>
-		<header className="bg-dark p-5 border-bottom border-primary">
-			<h1 className="text-light">Employee Directory</h1>
-		</header>
-		<main>
-			<section className="d-flex flex-column align-items-center px-3 px-md-5">
-				<App />
-			</section>
-		</main>
-		<footer></footer>
+		<Header />
+		<Main app={<App />} />
+		<Footer />
 	</>,
 
 	document.getElementById("root")
