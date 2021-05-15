40561ff47d08e82402c547864ee6a7e0ef29f852
Javier Perez <javier21perez98@gmail.com>
2021-05-14 23:03:05 -0700

    added a sort button

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,11 +6,27 @@ function Table(props) {
 			<table className="table">
 				<thead className="thead-light text-center">
 					<tr>
-						<th scope="col">Image</th>
-						<th scope="col">Name</th>
-						<th scope="col">Phone</th>
-						<th scope="col">Email</th>
-						<th scope="col">DOB: MM/DD/YY</th>
+						<th scope="col" className="text-dark">
+							Image
+						</th>
+						<th scope="col">
+							<button
+								type="button"
+								className="btn btn-link p-0 text-decoration-none text-dark font-weight-bold"
+								onClick={props.handleSort}
+							>
+								Name <i className="fa fa-sort" />
+							</button>
+						</th>
+						<th scope="col" className="text-dark">
+							Phone
+						</th>
+						<th scope="col" className="text-dark">
+							Email
+						</th>
+						<th scope="col" className="text-dark">
+							DOB: MM/DD/YY
+						</th>
 					</tr>
 				</thead>
 				{props.row}
