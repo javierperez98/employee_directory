23c6aaebf277f3cc260c26273c3a5dcecf201658
Javier Perez <javier21perez98@gmail.com>
2021-05-15 10:40:18 -0700

    updated styles

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ function Search(props) {
 	return (
 		<div className="my-5 d-flex flex-row col-md-9 col-lg-6">
 			<label className="form-label m-0 d-flex align-items-center">
-				<h2 className="m-0 text-dark">Search: </h2>
+				<h2 className="m-0 text-dark pr-2">Search:</h2>
 			</label>
 			<input
 				onChange={props.handleFind}
