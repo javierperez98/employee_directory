4c7a6b29c8b9683c3595c1e8a5aca10f681c4b5e
Javier Perez <javier21perez98@gmail.com>
2021-05-15 10:39:22 -0700

    added a p tag for inctructions

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ function Header() {
 	return (
 		<header className="bg-dark p-5 border-bottom border-primary">
 			<h1 className="text-light">Employee Directory</h1>
+			<p className="text-light">
+				Search for Employees and order by name or email.
+			</p>
 		</header>
 	);
 }
