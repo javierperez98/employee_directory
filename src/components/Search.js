6be83112a4ef252f5225eb7197d93796412a1cfa
Javier Perez <javier21perez98@gmail.com>
2021-05-14 20:52:08 -0700

    fixed errors

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,6 @@ function Search(props) {
 	return (
 		<div className="d-flex flex-row">
 			<input
-				value={props.find}
 				onChange={props.handleFind}
 				className="form-control form-control-lg mx-auto my-5"
 				type="text"
